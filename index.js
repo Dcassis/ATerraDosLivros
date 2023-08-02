919e00c9c579fda4ab8c31ef7e34dae7e9af2a5b
Daniel Carvalho <dan.c.assis@gmail.com>
2023-08-01 21:25:42 -0300

    fotos

--- index.js
+++ index.js
@@ -32,3 +32,54 @@ function myFunction1() {
   moreText_1.style.display = "inline";
   }
 }
+
+function myFunction2() {  
+  let dots_2 = document.getElementById("dots2");
+  let moreText_2 = document.getElementById("more2");
+  let btnText_2 =document.getElementById("myBtn2");
+  let plusminus_2 = document.getElementById("btnChange2");
+
+  if (dots_2.style.display === "none") {
+  dots_2.style.display = "inline";
+  plusminus_2.innerHTML = "mais...";						
+  moreText_2.style.display = "none";
+  } else {
+  dots_2.style.display = "none";
+  plusminus_2.innerHTML = "menos...";
+  moreText_2.style.display = "inline";
+  }
+}
+
+function myFunction3() {  
+  let dots_3 = document.getElementById("dots3");
+  let moreText_3 = document.getElementById("more3");
+  let btnText_3 =document.getElementById("myBtn3");
+  let plusminus_3 = document.getElementById("btnChange3");
+
+  if (dots_3.style.display === "none") {
+  dots_3.style.display = "inline";
+  plusminus_3.innerHTML = "mais...";						
+  moreText_3.style.display = "none";
+  } else {
+  dots_3.style.display = "none";
+  plusminus_3.innerHTML = "menos...";
+  moreText_3.style.display = "inline";
+  }
+}
+
+function myFunction4() {  
+  let dots_4 = document.getElementById("dots4");
+  let moreText_4 = document.getElementById("more4");
+  let btnText_4 =document.getElementById("myBtn4");
+  let plusminus_4 = document.getElementById("btnChange4");
+
+  if (dots_4.style.display === "none") {
+  dots_4.style.display = "inline";
+  plusminus_4.innerHTML = "mais...";						
+  moreText_4.style.display = "none";
+  } else {
+  dots_4.style.display = "none";
+  plusminus_4.innerHTML = "menos...";
+  moreText_4.style.display = "inline";
+  }
+}
